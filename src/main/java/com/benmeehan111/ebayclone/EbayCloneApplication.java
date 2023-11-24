2bf9a64d47292708867d7a6a3cfd89320f4efb1b
Ben Meehan <benmeehan111@example.com>
2023-11-24 17:44:15 +0530

    added db connection info

---
+++ src/main/java/com/benmeehan111/ebayclone/EbayCloneApplication.java
@@ -0,0 +1,13 @@
+package com.benmeehan111.ebayclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benmeehan111/ebayclone/EbayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benmeehan111.ebayclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
