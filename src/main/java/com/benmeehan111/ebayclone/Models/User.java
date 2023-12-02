d60f63db47bebb2e58736dfa88fa8b6017ee3d7b
Ben Meehan <benmeehan111@gmail.com>
2023-12-02 16:13:29 +0530

    updating persistence imports

--- src/main/java/com/benmeehan111/ebayclone/Models/User.java
+++ src/main/java/com/benmeehan111/ebayclone/Models/User.java
@@ -4,7 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
 @Entity
